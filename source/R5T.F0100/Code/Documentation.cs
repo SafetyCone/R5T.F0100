5317fc7e5a050d71f33da8b0e20d5674e13517a0
davidcoats <david@getsafetycone.com>
2023-01-28 15:12:11 -0800

    Initial commit.

---
+++ source/R5T.F0100/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0100
+{
+	/// <summary>
+	/// Razor component (Blazor component) related functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
